20021fde21ad530e53910fe97810d0c8e049dbcc
Richard Albert Leddy <richardaleddy@gmail.com>
2021-03-02 14:50:33 -0800

    Update persistence_category_server.js
    Customize asset paths for the application.

--- services/endpoints/persistence_category_server.js
+++ services/endpoints/persistence_category_server.js
@@ -2,6 +2,7 @@
 const PersistenceMessageEndpoint = require("categorical-handlers/persistence.js")
 //
 const fs = require('fs')
+const fsPromises = require('fs/promises')
 
 class TransitionsPersistenceEndpoint extends PersistenceMessageEndpoint {
     constructor(conf) {
@@ -10,37 +11,109 @@ class TransitionsPersistenceEndpoint extends PersistenceMessageEndpoint {
     //
 
     make_path(u_obj) {
-console.dir(u_obj)
-        if ( u_obj.param ) {
-console.dir(u_obj.param)
-            let key_field = u_obj.param.key_field
-            let asset_info = u_obj.param[key_field]   // dashboard+striking@pp.com
-            asset_info = asset_info.split('+')
-            let user_path = this.user_directory
-            let user_id = asset_info.pop()
-            //
-            user_path += '/' + user_id
-            let entry_type = asset_info.pop()
-            user_path += '/' + entry_type
-            //
-            if ( asset_info.length ) {
+        let key_field = u_obj.param.key_field
+        let asset_info = u_obj.param[key_field]   // dashboard+striking@pp.com
+        asset_info = asset_info.split('+')
+        let user_path = this.user_directory
+        let user_id = asset_info.pop()
+        //
+        user_path += '/' + user_id
+        let entry_type = asset_info.pop()
+        user_path += '/' + entry_type
+        //
+        if ( asset_info.length ) {
+            let file = asset_info.pop()
+            user_path += '/' + file + ".json"
+        } else {
+            user_path += ".json"
+        }
+console.log(user_path)
+        return(user_path)
+    }
+
+    // ----
+    async user_action_keyfile(op,obj) {
+        switch ( op ) {
+            case 'C' : {
+                let tpath = obj.trans_path
+                asset_info = tpath.split('+')
+                //
+                let user_path = this.user_directory
+                let user_id = asset_info.pop()
+                user_path += '/' + user_id
+                //
+                let entry_type = asset_info.pop()
+                user_path += '/' + entry_type
+                //
+                let entries_file = user_path + ".json"
+                let entries_record = await fsPromises.readFile(entries_file)
+                entries_record = JSON.parse(entries_record.toString())
+                //
                 let file = asset_info.pop()
                 user_path += '/' + file + ".json"
-            } else {
-                user_path += ".json"
+                //
+                obj.file_name = user_path
+                if ( entries_record.entries[entry_type] === undefined ) {
+                    entries_record.entries[entry_type] = []
+                }
+                entries_record.entries[entry_type].push(obj)
+                entries_record = JSON.stringify(entries_record)
+                await fsPromises.writeFile(entries_file,entries_record)
+                break;
             }
-console.log(user_path)
-            //
-            return(user_path)
-        } else {
-            let entry_type = u_obj.asset_type
-            let user_id = u_obj._id
-            let user_path = this.user_directory + '/' + user_id + '/'
-            user_path += entry_type + '/' + u_obj[u_obj.key_field] + ".json"
-            return(user_path)    
+            case 'U' : {
+                let tpath = obj.trans_path
+                asset_info = tpath.split('+')
+                //
+                let user_path = this.user_directory
+                let user_id = asset_info.pop()
+                user_path += '/' + user_id
+                //
+                let entry_type = asset_info.pop()
+                user_path += '/' + entry_type
+                //
+                let entries_file = user_path + ".json"
+                let entries_record = await fsPromises.readFile(entries_file)
+                entries_record = JSON.parse(entries_record.toString())
+                //
+                let file = asset_info.pop()
+                user_path += '/' + file + ".json"
+                //
+                obj.file_name = user_path
+                if ( entries_record.entries[entry_type] !== undefined ) {
+                    let entry_list = entries_record.entries[entry_type]
+                    for ( let i = 0; i < entry_list.length; i++ ) {
+                        let entry = entry_list[i]
+                        if ( entry.id == obj.id ) {
+                            entry_list[i] = obj
+                        }
+
+                    }
+                }
+                entries_record = JSON.stringify(entries_record)
+                await fsPromises.writeFile(entries_file,entries_record)
+                break;
+            }
+            case 'D' : {
+                break;
+            }
         }
+        /*
+{
+    "id" : Math.floor(Math.random()*5000),
+    "published" : (Math.random()>0.667),
+    'title' : `${ky}_${i}_a bunch of stuff that can be said.....`,
+    'data'  : `All about ${ky}_${i}_a bunch of stuff that can be said..... if you will\on a better day something random`,
+    'deleted' : false,
+    'saved_ever' : true,
+    'asset_type' : ky
+}
+        */
     }
 }
+
+
+
 
 
 let conf_file = 'relay-service.conf'
